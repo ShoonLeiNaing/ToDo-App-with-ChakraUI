9b64f2fb5a6210ae31862250210ad8352f358a82
ShoonLeiNaing <pandapanda254.pp@gmail.com>
2021-07-14 16:41:28 +0630

    Heading done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { Heading, VStack, IconButton } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={5}>
+      <IconButton
+        isRound="true"
+        size="lg"
+        icon={<FaSun />}
+        alignSelf="flex-end"
+      />
+      <Heading
+        mb="8"
+        fontWeight='extrabold' 
+        size="2xl"
+        bgGradient="linear(to-r,pink.500,pink.300,blue.500)"
+        bgClip="text"
+      >
+        Todo Application
+      </Heading>
+    </VStack>
   );
 }
 
---
+++ src/components/AddTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddTodo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddTodo
